318e45d534f7ac52017e5f1adfd972e6a1cf2310
AndrePassoni <andre.magalhaes.passoni@gmail.com>
2026-02-10 11:25:16 -0300

    showing the quantity of expenses

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ const amount = document.getElementById("amount")
 const expense = document.getElementById("expense")
 const category = document.getElementById("category")
 const expenseList = document.querySelector("ul")
+const expensesQuantity = document.querySelector("aside header p span")
 
 amount.oninput = () => {
     let value = amount.value.replace(/\D+/g, "")
@@ -65,8 +66,21 @@ function expenseAdd(newExpense) {
         expenseInfo.append(expenseName, expenseCategory)
         expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
         expenseList.append(expenseItem)
+
+        updateTotals()
     } catch (error) {
         alert("Não foi possível atualizar a lista de despesas.")
+        console.log(error)
+    }
+}
+
+function updateTotals() {
+    try {
+        const items = expenseList.children
+
+        expensesQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`
+    } catch (error) {
+        alert("Não foi possível atualizar os totais.")
         console.log(error)
     }
 }
